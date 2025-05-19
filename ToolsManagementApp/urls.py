d73ed34d80a682214ccf5bdbe4b86e3d672c8329
kodem <kodem@kkwindsolutions.com>
2025-05-19 14:12:54 +0200

    develop app, new views, models and urls. Upgrade html and css

--- ToolsManagementApp/urls.py
+++ ToolsManagementApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ToolListView
+from .views import ToolListView, ToolCreateView, ToolUpdateView, ToolDeleteView, ToolDetailView
 
 
 app_name = 'tools'
@@ -7,5 +7,10 @@ app_name = 'tools'
 urlpatterns = [
     # GET  /tools/           → lista narzędzi
     path('', ToolListView.as_view(), name='tool-list'),
-
+    # GET  /tools/add/      → formularz dodawania narzędzia
+    path('add/', ToolCreateView.as_view(), name='tool-add'),
+    
+    path('<int:pk>/',    ToolDetailView.as_view(), name='tool-detail'),
+    path('<int:pk>/edit/',   ToolUpdateView.as_view(), name='tool-edit'),
+    path('<int:pk>/delete/', ToolDeleteView.as_view(), name='tool-delete'),
 ]
--- ToolsManagementApp/views.py
+++ ToolsManagementApp/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import AddTool
-from django.views.generic import ListView
-from django.views.generic import TemplateView
+from django.views.generic import ListView, CreateView, TemplateView, DetailView, UpdateView, DeleteView
 
+class ToolCreateView(CreateView):
+    model = AddTool
+    fields = ['name','description','quantity','price','image']
+    template_name = 'tools/tool_form.html'      # <— własny formularz
+    success_url = reverse_lazy('tools:tool-list')
+    
 class ToolListView(ListView):
     model = AddTool
-    template_name = 'tools_list.html'
-    context_object_name = 'tools'
+    context_object_name = 'tools'               # w template: użyjesz {{ tools }}
+    template_name = 'tools/tool_list.html' 
     
+class ToolDetailView(DetailView):
+    model = AddTool
+    context_object_name = 'tool'                # w template: użyjesz {{ tool }}
+    template_name = 'tools/tool_detail.html'     # <— własny szablon
+    
+class ToolUpdateView(UpdateView):
+    model = AddTool
+    fields = ['name','description','quantity','price','image']
+    template_name = 'tools/tool_form.html'      # <— własny formularz
+    success_url = reverse_lazy('tools:tool-list')
+    
+class ToolDeleteView(DeleteView):
+    model = AddTool
+    template_name = 'tools/tool_confirm_delete.html'
+    success_url = reverse_lazy('tools:tool-list')
     
 class HomePageView(TemplateView):
     template_name = 'home.html'
